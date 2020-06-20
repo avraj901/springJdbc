9187d0c40610cb4a91c8864728c01408bff00183
avraj901 <avraj901@gmail.com>
2020-06-20 13:44:16 -0400

    insert and Fetch EmployeeDetails with spring Jdbc

---
+++ SpringJdbcCrudOperation-insertandfetchEmployeeDetails/src/com/nt/service/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.nt.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.nt.dao.EmployeeDAO;
+import com.nt.vo.EmployeeVO;
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+	private EmployeeDAO employeeDAO;
+	public void setEmployeeDAO(EmployeeDAO employeeDAO) {
+		this.employeeDAO = employeeDAO;
+	}
+	@Override
+	public int insertEmployee(EmployeeVO employeeVO) throws SQLException {
+		return employeeDAO.createEmployee(employeeVO);
+	
+	}
+
+	@Override
+	public EmployeeVO fetchEmployeeDetails(String email) {
+	  
+		return employeeDAO.getEmployeeDetails(email);
+	}
+
+	@Override
+	public void deleteEmployeeByName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateEmployeeByEmail(String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<EmployeeVO> informationOfEmployeee(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
